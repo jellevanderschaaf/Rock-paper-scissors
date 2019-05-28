475e48c6b9dd640d5117cd814ab401823911f73e
jellevanderschaaf <50097870+jellevanderschaaf@users.noreply.github.com>
2019-05-28 14:58:43 +0200

    Select which champion to send to the arena.

--- javascript.js
+++ javascript.js
@@ -1,12 +1,34 @@
 // Global variables
 
-// var characterConan = "Conan";
-// var characterThulsaDoom = "Thulsa Doom";
-// var characterZula = "Zula";
-// var characterRexor = "Rexor";
+var playerCharacter;
+
+var characterConan = "Conan";
+var characterThulsaDoom = "Thulsa Doom";
+var characterZula = "Zula";
+var characterRexor = "Rexor";
 
 // functions character selection
 
 function chooseCharacter(id) {
-    document.getElementById(id).style.border = "thick dashed rgb(121, 206, 3)";
+    document.getElementById(id).style.border = "thick solid #00C851";
+    playerCharacter = id;
+
+    if (id == 'Conan') {
+        document.getElementById('Thulsa').style.border = "";
+        document.getElementById('Zula').style.border = "";
+        document.getElementById('Rexor').style.border = "";
+    } else if (id == 'Thulsa') {
+        document.getElementById('Conan').style.border = "";
+        document.getElementById('Zula').style.border = "";
+        document.getElementById('Rexor').style.border = "";
+    } else if (id == 'Zula') {
+        document.getElementById('Conan').style.border = "";
+        document.getElementById('Thulsa').style.border = "";
+        document.getElementById('Rexor').style.border = "";
+    } else if (id == 'Rexor') {
+        document.getElementById('Conan').style.border = "";
+        document.getElementById('Thulsa').style.border = "";
+        document.getElementById('Zula').style.border = "";
+    }
+
 };
