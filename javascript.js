b9b6c26891aa584d325f54475b5ea2e5956b8842
jellevanderschaaf <50097870+jellevanderschaaf@users.noreply.github.com>
2019-05-28 12:17:11 +0200

    Changing the portrait borders onclick.

---
+++ javascript.js
@@ -0,0 +1,12 @@
+// Global variables
+
+// var characterConan = "Conan";
+// var characterThulsaDoom = "Thulsa Doom";
+// var characterZula = "Zula";
+// var characterRexor = "Rexor";
+
+// functions character selection
+
+function chooseCharacter(id) {
+    document.getElementById(id).style.border = "thick dashed rgb(121, 206, 3)";
+};
